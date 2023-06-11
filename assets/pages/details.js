d35127876070556dcca489fcfa3e199cb29bb974
valdeveloper <jjavaldz@gmail.com>
2023-06-11 00:46:16 -0400

    sprint 03

--- assets/pages/details.js
+++ assets/pages/details.js
@@ -1,34 +1,32 @@
-let div = document.getElementById("cointainerDetails");
+const $details = document.getElementById("containerDetails")
 
-function newTemplate(objeto){
-    return `<div class="pt-4 ps-2 pe-2">
+console.log(location.search)
+
+const params = new URLSearchParams(location.search)
+
+const _id = params.get("_id")
+
+const findedCard = data.events.find( element => element._id == _id)
+
+console.log(findedCard)
+
+$details.innerHTML = `
+            <div class="pt-4 ps-2 pe-2">
                 <div class="card mb-3 bg-black text-violet rounded-4" style="max-width: 768px;">
                     <div class="row g-0">
                         <div class="col-md-4 d-flex object-fit-contain">
-                            <img src="${objeto.image}" class="img-fluid rounded-start p-3 rounded-4 object-fit-cover" alt="${(objeto.name).replace(/ /g, "-")}">
+                            <img src="${findedCard.image}" class="img-fluid rounded-start p-3 rounded-4 object-fit-cover" alt="${(findedCard.name).replace(/ /g, "-")}">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h4 class="card-title">${objeto.name}</h4>
-                                <p class="card-text fs-5">${objeto.description}</p>
-                                <p class="card-text fs-5">${objeto.date}</p>
-                                <p class="card-text fs-5">${objeto.place}</p>
-                                <p class="card-text fs-5">${objeto.price}.0 USD.</p>
-                                <p class="card-text fs-5"><small class="text-violet">${objeto.capacity} P</small></p>
+                                <h4 class="card-title">${findedCard.name}</h4>
+                                <p class="card-text fs-5">${findedCard.description}</p>
+                                <p class="card-text fs-5">Date: ${findedCard.date}</p>
+                                <p class="card-text fs-5">Place: ${findedCard.place}</p>
+                                <p class="card-text fs-5">Price: ${findedCard.price}.0 USD.</p>
+                                <p class="card-text fs-5"><small class="text-violet">Capacity: ${findedCard.capacity} P</small></p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>`
-}
-
-function printData(array, place){
-    let template = ""
-    for (let event of array){
-        template += newTemplate(event)
-    }
-    console.log(template)
-    place.innerHTML += template
-}
-
-printData(data.events, cointainerDetails)
